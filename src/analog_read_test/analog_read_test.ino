4a36df5e284a0f5dfbdd41b667c4c6b5b539b991
kds <kds@mitre.org>
2016-09-26 17:42:46 -0400

    Added PCB Layout for demo board

---
+++ src/analog_read_test/analog_read_test.ino
@@ -0,0 +1,16 @@
+void setup()
+{                
+  Serial.begin(38400);
+  analogReference(EXTERNAL);
+  analogReadResolution(12);
+  
+}
+
+int val;
+
+void loop()                     
+{
+  val = analogRead(0);
+  Serial.print("analog 0 is: ");
+  Serial.println(val);
+}
